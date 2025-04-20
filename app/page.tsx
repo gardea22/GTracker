7152da8b6ac46b3eb3b88b8d57937ad6511ec6b6
egagardea <73439054+gardea22@users.noreply.github.com>
2025-04-20 14:48:54 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,3 @@
-import React from 'react';
-
-const Dashboard = () => {
-  return (
-    <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
-      <h1 style={{ textAlign: 'left', color: '#4A90E2' }}>GTracker</h1>
-     
-    </div>
-  );
-};
-
-export default Dashboard;
-
-import React, { useState } from "react";
 import { FaPencilAlt } from "react-icons/fa";
 
 const AirdropFormPopup = () => {
