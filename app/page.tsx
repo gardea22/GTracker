5d0a55226802289f8fba3aa0ca47a2e3639f88e9
egagardea <73439054+gardea22@users.noreply.github.com>
2025-04-20 14:36:06 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import React from 'react';
 const Dashboard = () => {
   return (
     <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
-      <h1 style={{ textAlign: 'center', color: '#4A90E2' }}>Airdrop Tracker Dashboard</h1>
-      <p style={{ textAlign: 'center', marginBottom: '30px' }}>
-        Kelola dan analisis airdrop Anda dengan mudah!
-      </p>
+      <h1 style={{ textAlign: 'left', color: '#4A90E2' }}>GTracker</h1>
+     
       <div style={{ display: 'flex', justifyContent: 'space-around' }}>
         <div style={{ border: '1px solid #ddd', borderRadius: '10px', padding: '20px', width: '30%' }}>
           <h2>Tracker</h2>
